572a5e6fe56b9b8c5723e228f4c5329a922820c8
grandfather who does not understand anything <91083939+rufus014@users.noreply.github.com>
2021-12-05 16:23:29 +0300

    Delete Проект directory

--- Проект/На полку/Books/Комментарий.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("<p>"+ $("#userName").val() + " (" + $("#rating").val()+ ")" + ": " + $("#textarea").val() + "</p>").appendTo("#com_area");
-    });
-});
